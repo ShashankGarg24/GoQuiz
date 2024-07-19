e6fc97235659c466cdd97c5617edf36b27939cca
unknown <sg24101999@gmail.com>
2024-07-20 02:12:05 +0530

    get user input and compare with answer for scorecard

--- quiz/main.go
+++ quiz/main.go
@@ -9,5 +9,18 @@ func main() {
 	quizFileName := utils.FetchFileName()
 	quizFile := utils.LoadFile(quizFileName)
 	problems := utils.LoadProblemsFromFile(quizFile)
-	fmt.Print(problems)
+
+	correctAnswers := 0
+	for i, problem := range problems {
+		fmt.Printf("Problem #%d: %s\n", (i + 1), problem.Question)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == problem.Answer {
+			fmt.Printf("Correct Answer!\n")
+			correctAnswers++
+		} else {
+			fmt.Printf("Incorrect Answer!\n")
+		}
+	}
+	fmt.Printf("You scored %d out of %d.", correctAnswers, len(problems))
 }
