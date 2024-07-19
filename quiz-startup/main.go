3538a2a96946649eb6e8af09b229887328b1716c
unknown <sg24101999@gmail.com>
2024-07-19 14:43:55 +0530

    fetching file from cmd and loading it

--- quiz-startup/main.go
+++ quiz-startup/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
 func main() {
-	
+	quizFileName := fetchFileName()
+	quizFile := loadFile(quizFileName)
+	fmt.Print(quizFile)
+
+}
+
+func fetchFileName() string {
+	fileName := flag.String("csv", "math-problems.csv", "Provide with a csv file of problems in the format of Question, Answer")
+	flag.Parse()
+	return *fileName
+}
+
+func loadFile(name string) *os.File {
+	file, err := os.Open("../quizes/" + name)
+	if err != nil {
+		gracefulExit(fmt.Sprintf("Error in opening the CSV file: %s", err))
+	}
+	return file
+}
+
+func gracefulExit(msg string) {
+	fmt.Printf(msg)
+	os.Exit(1)
 }
