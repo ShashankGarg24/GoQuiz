45a240ad25c8eb745fdcbd0e2581b578ee950ee6
unknown <sg24101999@gmail.com>
2024-08-04 03:11:17 +0530

    timer added using channel and go routine

--- quiz/constants/consts.go
+++ quiz/constants/consts.go
@@ -4,6 +4,6 @@ const (
 	DEFAULT_FILE = "math-problems.csv"
 	QUIZ_PATH    = "../resource/"
 	CSV_HELP     = "Provide with a csv file of problems in the format of Question, Answer."
-	DEFAULT_TIME = 20
+	DEFAULT_TIME = 10
 	TIME_HELP    = "Provide with a time limit for the quiz in seconds."
 )
--- quiz/main.go
+++ quiz/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"GoQuiz/quiz/utils"
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -11,16 +12,31 @@ func main() {
 	quizFile := utils.LoadFile(quizFileName)
 	problems := utils.LoadProblemsFromFile(quizFile)
 
+	timer := time.NewTimer(time.Duration(quizTimeLimit) * time.Second)
 	correctAnswers := 0
 	for i, problem := range problems {
 		fmt.Printf("Problem #%d: %s\n", (i + 1), problem.Question)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == problem.Answer {
-			fmt.Printf("Correct Answer!\n")
-			correctAnswers++
-		} else {
-			fmt.Printf("Incorrect Answer!\n")
+
+		answerChannel := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerChannel <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Printf("Time's up!\n")
+			fmt.Printf("You scored %d out of %d.", correctAnswers, len(problems))
+			return
+		case answer := <- answerChannel:
+			
+			if answer == problem.Answer {
+				fmt.Printf("Correct Answer!\n")
+				correctAnswers++
+			} else {
+				fmt.Printf("Incorrect Answer!\n")
+			}
 		}
 	}
 	fmt.Printf("You scored %d out of %d.", correctAnswers, len(problems))
